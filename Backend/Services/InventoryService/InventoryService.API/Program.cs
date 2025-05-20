5e087bc9e9e04c2b393601bc2af50aae4010a1c9
Aung Naing Tun <aungnaing.tun@partnerassociates.com>
2025-05-20 23:50:38 +0630

    implement grpc (order,inventory) services

--- Backend/Services/InventoryService/InventoryService.API/Program.cs
+++ Backend/Services/InventoryService/InventoryService.API/Program.cs
@@ -1,13 +1,24 @@
+using InventoryService.Application.Services.gRPC;
 using InventoryService.InfraStructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
+builder.Services.AddGrpc();
 builder.Services.AddControllers();
 builder.Services.AddDbContext<InventoryDbContext>(options =>
  options.UseNpgsql(builder.Configuration.GetConnectionString("LATALL_Inventory_Connection")));
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5004, listenOptions =>
+    {
+        listenOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+    });
+});
+
 var app = builder.Build();
+app.MapGrpcService<InventoryServiceImpl>();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
---
+++ Backend/Services/InventoryService/InventoryService.Application/Services/gRPC/InventoryServiceImpl.cs
@@ -0,0 +1,47 @@
+﻿using Grpc.Core;
+using InventoryService.Grpc;
+using InventoryService.InfraStructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace InventoryService.Application.Services.gRPC
+{
+    public class InventoryServiceImpl(InventoryDbContext context, ILogger<InventoryServiceImpl> logger) : Inventory.InventoryBase
+    {
+        private readonly InventoryDbContext _context = context;
+        private readonly ILogger<InventoryServiceImpl> _logger = logger;
+
+        public async override Task<StockResponse> GetStock(StockRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation("GetStock GRPC {ProductId}", request.ProductId);
+            var stock = await _context.Stocks.FirstOrDefaultAsync(s => s.ProductId == request.ProductId);
+            var response = new StockResponse { ProductId = request.ProductId, Quantity = stock!.Quantity };
+            return response;
+        }
+
+        public async override Task<StockBatchResponse> GetStockBatch(StockBatchRequest request, ServerCallContext context)
+        {
+            var response = new StockBatchResponse();
+
+            var productIds = request.ProductIds.ToList();
+            _logger.LogInformation("GetStockBatch GRPC with productIDs {ProductId}", productIds);
+            var stocks = await _context.Stocks
+                .Where(s => productIds.Contains(s.ProductId))
+                .ToListAsync();
+
+            foreach (var id in productIds)
+            {
+                var stock = stocks.FirstOrDefault(s => s.ProductId == id);
+                response.Stocks.Add(new StockResponse
+                {
+                    ProductId = id,
+                    Quantity = stock?.Quantity ?? 0
+                });
+            }
+
+            return response;
+        }
+
+
+    }
+}
