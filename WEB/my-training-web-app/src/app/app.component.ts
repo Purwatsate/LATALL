70c524fdb6eb5e98ca22699f0201af305aa81b04
Aung Naing Tun <aungnaing.tun@partnerassociates.com>
2025-05-23 00:04:39 +0630

    test angular signal

--- WEB/my-training-web-app/src/app/app.component.ts
+++ WEB/my-training-web-app/src/app/app.component.ts
@@ -1,58 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
-import { ChangeDetectionStrategy, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
-import {
-  FormBuilder,
-  FormControl,
-  FormsModule,
-  ReactiveFormsModule,
-} from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import {
-  FloatLabelType,
-  MatFormFieldModule,
-} from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { map } from 'rxjs/operators';
+import { inject } from '@angular/core';
 import { ProductService } from './features/products/services/product.service';
-import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatSlideToggleModule,
-    MatButtonModule,
-    MatDividerModule,
-    MatIconModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatCheckboxModule,
-    MatRadioModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatIconModule,
-  ],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   private productService = inject(ProductService);
+  isLoading = signal(false);
 
   ngOnInit() {}
 
   callAPI() {
-    const products = lastValueFrom(
-      this.productService.getProducts('20.63799', '93.1637', 1, 5)
-    );
-    console.log(products);
+    this.isLoading.set(true);
+    setTimeout(() => {
+      this.productService.getProducts('20.63799', '93.1637', 1, 5).subscribe({
+        next: (data) => {
+          this.isLoading.set(false);
+          console.log(data);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
+    }, 4000);
   }
 }
